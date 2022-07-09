a7568c9a08a84ff4d6265689f7bfacdab5e27721
segoSambel <habibhusain313@gmail.com>
2022-07-09 15:29:18 +0700

    update: adding required features for secant page

--- pages/terbuka/secant.tsx
+++ pages/terbuka/secant.tsx
@@ -11,6 +11,7 @@ import Tombol from "../../components/tombol";
 import LoopInput from "../../components/loop_input";
 import Secant from "../../src/terbuka/secant";
 import SecantFunc from "../../components/calculations/secant_func";
+import { getRandom } from "../../src/formula";
 
 function calculate(
   iteration: number,
@@ -34,7 +35,9 @@ function calculate(
 const SecantPage: NextPage = () => {
   const [loop, setLoop] = useState<number>(0);
   const [data, setData] = useState<Secant[]>([]);
-  const [formErr, setFormErr] = useState(true);
+  const [formErr, setFormErr] = useState<boolean>(true);
+  const [firstGuessNumber, setFirstGuessNumber] = useState<number>(2);
+  const [secondGuessNumber, setSecondGuessNumber] = useState<number>(3);
 
   const handleClick = () => {
     if (!formErr) {
@@ -52,6 +55,11 @@ const SecantPage: NextPage = () => {
     setLoop(value);
   };
 
+  const getRandomHandler = () => {
+    setFirstGuessNumber(getRandom());
+    setSecondGuessNumber(getRandom());
+  };
+
   return (
     <>
       <Header title="Metode Secant"></Header>
@@ -61,13 +69,31 @@ const SecantPage: NextPage = () => {
           <h1 className="text-center font-bold text-3xl my-20 mb-4">
             Metode Secant
           </h1>
-          <div className="justify-center mb-5 flex">
-            <p className="mr-3">Rumus Secant:</p>
-            <SecantFunc></SecantFunc>
-          </div>
-          <div className="flex justify-center items-center">
-            <LoopInput formErr={formErr} validateHandler={validate}></LoopInput>
-            <Tombol clickHandler={handleClick} text="Hitung Sekarang!"></Tombol>
+          <div className="flex justify-center mb-5">
+            <div className="flex flex-col">
+              <span>
+                Rumus Secant : <SecantFunc></SecantFunc>
+              </span>
+              <div className="my-2">
+                <h3 className="inline mr-3">
+                  Tebakan Awal : {`[${firstGuessNumber},${secondGuessNumber}]`}
+                </h3>
+                <Tombol
+                  clickHandler={getRandomHandler}
+                  text="Ubah Tebakan Awal"
+                ></Tombol>
+              </div>
+              <div className="flex">
+                <LoopInput
+                  formErr={formErr}
+                  validateHandler={validate}
+                ></LoopInput>
+                <Tombol
+                  clickHandler={handleClick}
+                  text="Hitung Sekarang!"
+                ></Tombol>
+              </div>
+            </div>
           </div>
         </section>
         <section className="px-4 pb-10">
