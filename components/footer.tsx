5a630f6a6f8f7319bdf7f3b4b4a8b21393a3b3e8
segoSambel <habibhusain313@gmail.com>
2022-07-18 10:08:04 +0700

    adding NIM

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-400 text-center bottom-0 w-full h-16 absolute">
-      <div className="text-center p-4">© 2022 Habib Husain Amirullah</div>
+      <div className="text-center p-4">
+        © 2022 Habib Husain Amirullah (201080200028)
+      </div>
     </footer>
   );
 }
