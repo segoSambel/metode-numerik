c874dd24eea2c06287028de4b0fa37fde9068fbf
Reyhan Haqiqi Alif F <saifunaris@gmail.com>
2022-10-21 23:08:22 +0700

    add new text in README.md & add new comment in index homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,33 +5,36 @@ import Navbar from "../components/navbar/navbar";
 import { routeTable } from "../src/route";
 import Footer from "../components/footer";
 
+// Index of Home Page
 const Home: NextPage = () => {
-  return (
-    <>
-      <Header title="Metode Numerik | Home"></Header>
-      <Navbar></Navbar>
-      <main>
-        <section className="container">
-          <h1 className="text-center font-bold text-3xl m-20 mb-10">
-            Aplikasi Penghitung SPNL
-          </h1>
-          <div className="flex flex-row">
-            <div className="flex flex-wrap justify-center">
-              {routeTable.map((item, index) => (
-                <Link href={item.href} passHref key={index}>
-                  <button className="block box-border text-center border-2 mb-2 rounded-md w-1/4 p-5 m-7 hover:bg-gray-50">
-                    <h1 className="font-bold text-xl mb-3">{item.name}</h1>
-                    <p>{item.definition}</p>
-                  </button>
-                </Link>
-              ))}
-            </div>
-          </div>
-        </section>
-      </main>
-      <Footer></Footer>
-    </>
-  );
+    return (
+        <>
+            <Header title="Metode Numerik | Home"></Header>
+            <Navbar></Navbar>
+            <main>
+                <section className="container">
+                    <h1 className="text-center font-bold text-3xl m-20 mb-10">
+                        Aplikasi Penghitung SPNL
+                    </h1>
+                    <div className="flex flex-row">
+                        <div className="flex flex-wrap justify-center">
+                            {routeTable.map((item, index) => (
+                                <Link href={item.href} passHref key={index}>
+                                    <button className="block box-border text-center border-2 mb-2 rounded-md w-1/4 p-5 m-7 hover:bg-gray-50">
+                                        <h1 className="font-bold text-xl mb-3">
+                                            {item.name}
+                                        </h1>
+                                        <p>{item.definition}</p>
+                                    </button>
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                </section>
+            </main>
+            <Footer></Footer>
+        </>
+    );
 };
 
 export default Home;
